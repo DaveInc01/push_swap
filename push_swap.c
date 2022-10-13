638520034fb667c17235f9d577eedcbd6791f57c
Davit Mkhitaryan <dmkhitar@c1r1s4.42yerevan.am>
2022-10-13 19:27:44 +0400

    Jack to Dave

--- push_swap.c
+++ push_swap.c
@@ -30,8 +30,6 @@ int	ft_atoi(const char *str)
 }
 
 
-
-
 int is_dublicate(int num, int *arr, int arr_size)
 {
 	int	i;
@@ -46,25 +44,87 @@ int is_dublicate(int num, int *arr, int arr_size)
 	return (0);
 }
 
-int pushing_to_arr(char *elem, int *unordered, int stack_size)
+int pushing_to_arr(char *elem, int *unordered)
 {
 	static int i;
 	int num = ft_atoi(elem);
-	if (num >= 2147483647 || is_dublicate(num, unordered, stack_size))
+	if (num >= 2147483647 || is_dublicate(num, unordered, i))
 		return (0);
 	unordered[i] = num;
 	i++;
 	return (1);
 }
 
+void swap_arr(int *elem1, int *elem2)
+{
+	int tmp;
+
+	tmp = *elem1;
+	*elem1 = *elem2;
+	*elem2 = tmp;
+}
+
+int *order_arr(int *unordered, int size)
+{
+	int	i;
+	int flag;
+
+	i = 0;
+	flag = 0;
+	--size;
+	while(i < size)
+	{
+		if(unordered[i] > unordered[i + 1])
+		{
+			flag = 1;
+			swap_arr(&unordered[i], &unordered[i + 1]);
+		}
+		i++;
+		if(i == size && flag == 1)
+		{
+			i = 0;
+			flag = 0;
+		}
+	}
+	return (unordered);
+}
+
+
+void fill_stack(t_data *list, char *elem, int *ordered_arr, int size)
+{
+	int	i;
+	int	num;
+	t_list e;
+
+	list = NULL;
+	i = 0;
+	num = ft_atoi(elem);
+	while (i < size)
+	{
+		if (num == ordered_arr[i])
+		{
+			// printf("okkkkk\n");
+			e = malloc(sizeof(t_list));
+			e = list->a;
+			list->a = list->a->next;
+			break;
+		}
+		i++;
+	}
+}
+
 int main(int argc, char **argv)
 {
+	int		i;
+	int		j;
+	int		*unordered;
+	int		*ordered;
+	int		stack_size;
+	t_data	*list;
+
+	list = malloc(sizeof(t_data));
 	if (argc >= 2)
 	{
-		int	i;
-		int	j;
-		int	*unordered;
-		int stack_size;
 
 		i = 1;
 		stack_size = argc - 1;
@@ -85,20 +145,26 @@ int main(int argc, char **argv)
 					return (0);
 				}
 			}
-			if(!(pushing_to_arr(argv[i], unordered, stack_size)))
+			if(!(pushing_to_arr(argv[i], unordered)))
 			{
 				write(1, "Error\n", 6);
 				return (0);
 			}
 			i++;
-			// order_arr(unordered)
 		}
-		i = 0;
-		while(i < stack_size)
+		ordered = order_arr(unordered, stack_size);
+		free(unordered);
+		i = 1;
+		while (argv[i])
 		{
-			printf("%d\n", unordered[i]);
+			fill_stack(list, argv[i], ordered, stack_size);
 			i++;
 		}
+		// while(i < stack_size)
+		// {
+		// 	printf("%d\n", ordered[i]);
+		// 	i++;
+		// }
 	}
 	return 0;
 }
